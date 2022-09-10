ed81888b720451ee5d6dac2c1135443d9f43492d
Stanislav <stas.kreyzo@gmail.com>
2022-09-10 19:52:34 +0700

    Поднял сервер

--- src/app.js
+++ src/app.js
@@ -1,19 +1,21 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const path = require('path');
-const express = require('express');
-const morgan = require('morgan');
-const session = require('express-session');
-const FileStore = require('session-file-store')(session);
+const path = require("path");
+const express = require("express");
+const morgan = require("morgan");
+const session = require("express-session");
+const FileStore = require("session-file-store")(session);
 
-// const { sequelize } = require('../db/models');
-const renderTemplate = require('../lib/renderReactModule');
+const renderTemplate = require("../lib/renderReactModule");
+const dbConnectionCheck = require("../db/dbConnectionCheck");
 
+const Main = require("../views/Main");
+dbConnectionCheck();
 const app = express();
 
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 // Чтобы наши статические файлы были видны браузеру, мы должны их подключить
-app.use(express.static(path.join(__dirname, '../public/')));
+app.use(express.static(path.join(__dirname, "../public/")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -21,9 +23,9 @@ app.use(express.urlencoded({ extended: true }));
 const { PORT, SESSION_SECRET } = process.env;
 
 const sessionConfig = {
-  name: 'your coockie name', // * Название куки
+  name: "your coockie name", // * Название куки
   store: new FileStore(), // * подключение стора (БД для куки) для хранения
-  secret: SESSION_SECRET ?? 'your key', // * ключ для шифрования куки
+  secret: SESSION_SECRET ?? "your key", // * ключ для шифрования куки
   resave: false, // * если true, пересохраняет сессию, даже если она не поменялась
   saveUninitialized: false, // * Если false, куки появляются только при установке req.session
   cookie: {
@@ -34,12 +36,11 @@ const sessionConfig = {
 
 app.use(session(sessionConfig));
 
+app.get("/", (req, res) => {
+  const user = req.session?.firstname;
+  renderTemplate(Main, { user }, res);
+});
+
 app.listen(PORT, async () => {
-  try {
-    await sequelize.authenticate();
-    console.log('Соединение с базой установлено!');
-  } catch (err) {
-    console.log(err, 'Error!');
-  }
   console.log(`Сервер поднят на ${PORT} порту!`);
 });
---
+++ views/Main.jsx
@@ -0,0 +1,10 @@
+const React = require("react");
+const Layout = require("./Layout");
+
+module.exports = function Main() {
+  return (
+    <Layout>
+      <p>Это главная страница</p>
+    </Layout>
+  );
+};
